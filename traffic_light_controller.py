91afe9603577abb141d2df23a2d6be8d2db9235a
Niya Paul <niyapaul610@gmail.com>
2025-07-20 18:52:40 +0530

    Added Python code for traffic light simulation

---
+++ traffic_light_controller.py
@@ -0,0 +1,18 @@
+# 🚦 Beginner Traffic Light Simulation (One cycle only)
+import time  # To add waiting/delay
+
+# Show RED light
+print("RED - STOP")
+time.sleep(3)  # Wait for 3 seconds
+
+# Show YELLOW light
+print("YELLOW - SLOW DOWN")
+time.sleep(2)  # Wait for 2 seconds
+
+# Show GREEN light
+print("GREEN - GO")
+time.sleep(3)  # Wait for 3 seconds
+
+
+# End of the cycle
+print("Traffic light cycle complete!")
